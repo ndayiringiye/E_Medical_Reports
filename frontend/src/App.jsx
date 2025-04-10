7404e5d347f98f8a16ee49bfc57045b4ea42bee7
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-04-10 18:28:33 +0200

    chore:/curently project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>h
+      <p className="text-4xl text-blue-700 underline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore repudiandae qui unde quia. Aut voluptate quis ea eum rerum ex nemo, nisi magnam repudiandae dolorum nesciunt ut vero expedita. Expedita?</p>
+    </div>
+  )
+}
+
+export default App
