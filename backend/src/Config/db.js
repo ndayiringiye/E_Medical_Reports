9210600ecd457d256c95b995dc9ce22c73490dda
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-05-15 11:21:57 +0200

    feat:/fix add start script for Render deployment

--- backend/src/Config/db.js
+++ backend/src/Config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectdb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI );
         console.log("database connected successfully :", conn.connection.host)
     } catch (error) {
         console.log("database connection failed :", error)
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,22 +4,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectdb } from "./Config/db.js";
 import userRouter from "./routes/userRoute.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
 dotenv.config();
 
 const app = express();
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const uploadsDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir, { recursive: true });
-}
-app.use('/uploads', express.static(path.join(__dirname, '../frontend/public/Download.mp4')));
 app.use(cors({
   origin: ["http://localhost:3000", "http://localhost:3001"],
   credentials: true,
@@ -27,7 +15,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(cookieParser());
-app.use(express.json());
 
 app.use("/api/user", userRouter);
 
